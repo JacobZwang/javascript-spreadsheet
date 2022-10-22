f1df025486f24b9dd2c6c3076112b6e0251e6c42
JacobZwang <zwang.jacob@gmail.com>
2022-10-22 14:41:08 -0700

    cell component & debug data display

---
+++ src/components/SheetCell.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  props: ["cell"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <input
+    v-model="cell.value"
+    @focus="() => (cell.isEditing = true)"
+    @blur="() => (cell.isEditing = false)"
+  />
+</template>
--- src/views/SheetView.vue
+++ src/views/SheetView.vue
@@ -1,19 +1,23 @@
 <script lang="ts">
+import Cell from "../components/SheetCell.vue";
+
 export default {
   data() {
     return {
       columns: Array.from({ length: 20 }, () => ({
-        width: 150,
+        width: 250,
       })),
       rows: Array.from({ length: 20 }, (_, i) => ({
         index: i,
         cells: Array.from({ length: 20 }, (_, j) => ({
           value: "",
           column: j,
+          isEditing: false,
         })),
       })),
     };
   },
+  components: { Cell },
 };
 </script>
 
@@ -27,7 +31,22 @@ export default {
     :key="row.index"
   >
     <div v-for="cell in row.cells" :key="cell.column">
-      <input v-model="cell.value" />
+      <Cell :cell="cell" />
+    </div>
+  </div>
+
+  <div
+    class="grid"
+    :style="{
+      gridTemplateColumns: columns.map((c) => c.width + 'px').join(' '),
+    }"
+    v-for="row in rows"
+    :key="row.index"
+  >
+    <div v-for="cell in row.cells" :key="cell.column" class="mb-2">
+      <div v-for="[key, value] in Object.entries(cell)" class="text-xs">
+        {{ key }}: {{ value }}
+      </div>
     </div>
   </div>
 </template>
